dcfacad6bc24b8c6ab287982741d1437df0f1c20
guilhemmarchand <guilhem.marchand@gmail.com>
2016-02-06 22:17:44 +0100

    Ignoring useless data from sphinx for docs

--- docs/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Nmon for Splunk - Performance monitor for Unix and Linux&#8217;s documentation!"],titleterms:{document:0,indic:0,linux:0,monitor:0,nmon:0,perform:0,splunk:0,tabl:0,unix:0,welcom:0}})
