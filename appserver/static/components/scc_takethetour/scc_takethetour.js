751b9b6f2ee5f38fc4bc76d940dcf348418db679
guilhemmarchand <guilhem.marchand@gmail.com>
2016-06-24 01:51:21 +0200

    Take the tour implementation, global design improvements

---
+++ appserver/static/components/scc_takethetour/scc_takethetour.js
@@ -0,0 +1,240 @@
+/*
+
+
+
+
+
+*/
+
+define(function(require, exports, module) {
+    var $ = require('jquery');
+	//var mvc = require('splunkjs/mvc');
+	var SimpleSplunkView = require('splunkjs/mvc/simplesplunkview');
+    var SplunkUtils = require('splunkjs/mvc/utils');
+	require("css!./scc_takethetour.css");
+	
+    var SccTakeTheTour = SimpleSplunkView.extend({
+        className: "scc-takethetour",
+		options: {
+			title: "Take the tour",
+			width: 60,
+			close_btn_label: "Close",
+			no_more_label: "Do not show this tour again",
+            hide_container: false,
+            show_credits: false
+        },
+
+        initialize: function() {
+			SimpleSplunkView.prototype.initialize.apply(this, arguments);
+            
+            //console.log(this.className);
+
+            // 
+            if(this.settings.get('hide_container') == true){
+                this.$el.parent().css({
+                    'padding':0,
+                    'margin':0,
+                    'height':'0px',
+                    'width':'0px'
+                })    
+            }
+            
+			return this;
+		},
+
+		render: function() {
+			this.$el.html('');
+
+            // 
+            var ck_name = 'scc_takethetour' + SplunkUtils.getCurrentApp();
+
+            function getCookie(ck_name) {
+				var oRegex = new RegExp("(?:; )?" + ck_name + "=([^;]*);?");
+				return oRegex.test(document.cookie) ? decodeURIComponent(RegExp["$1"]) : null;
+			}
+            
+			function setCookie(ck_name) {
+				document.cookie = ck_name + "= scc-takethetour";
+			}
+				
+			// GENERATE BOOTSTRAP MODAL
+			// ------------------------
+			if(typeof this.settings.get('user_slides') !== 'undefined' 
+				&& $('#'+this.settings.get('user_slides')).length == 1
+				&& getCookie(ck_name) == null){
+
+				var html_tpl = '<div class="scc-takethetour modal fade" id="scc-takethetour-modal" tabindex="-1" role="dialog" aria-labelledby="scc-takethetour-modal">'
+								+ 	'<div class="modal-dialog" role="document">'
+								+		'<div class="modal-content">'
+								+ 			'<div class="modal-header">'
+								+				'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+								+				'<h4 class="modal-title" id="myModalLabel">' + this.settings.get('title') + '</h4>'
+								+			'</div>'
+								+			'<div class="modal-body">'
+								+				'<div class="scc-takethetour-nav">'
+								+					'<button class="prev btn btn-default"><span class="caret"></span></button>'
+								+					'<div></div>'
+								+					'<button class="next btn btn-default"><span class="caret"></span></button>'
+								+ 				'</div>'
+								+				'<ul class="scc-takethetour-slides"></ul>'
+								+ 			'</div>'
+								+ 			'<div class="modal-footer">'
+								+				'<label class="never-show-again"><input type="checkbox">' + this.settings.get('no_more_label') + '</label>'
+								+				'<button type="button" class="btn btn-default" data-dismiss="modal">' + this.settings.get('close_btn_label') + '</button>'
+								+ 			'</div>'
+								+ 		'</div>'
+								+ 	'</div>'
+								+ '</div>';
+				this.$el.html(html_tpl);
+                
+				var $user_slides = $('#'+this.settings.get('user_slides')+' li');
+				var $modal = this.$el.find('#scc-takethetour-modal');
+				
+				$modal.css({width:this.settings.get('width') + "%", "margin-left": "-" + this.settings.get('width')/2 + "%"});
+				
+				var $modal_body = this.$el.find('#scc-takethetour-modal .modal-body');
+				var $slides = this.$el.find('.scc-takethetour-slides');
+				var $slides_nav = this.$el.find('.scc-takethetour-nav');
+				var $slides_nav_prev = $slides_nav.find('.prev');
+				var $slides_nav_next = $slides_nav.find('.next');
+				var $slides_nav_links = $slides_nav.find('div');
+				var $checkbox = this.$el.find('.never-show-again input');
+				
+				var active_slide_id = 0;
+				var max_slide_id = $user_slides.length - 1;
+
+                //
+                if(this.settings.get('show_credits') === true){
+                    var html_credits = '<div class="credits">'
+                                        +   'Powered by <a href="http://www.splunk.com" target="_blank">Splunk</a> and <a href="https://github.com/ftoulouse/splunk-components-collection" target="_blank">Scc</a>'
+                                        + '</div>';
+                    $modal.find('.modal-footer').append(html_credits);
+                }
+
+				
+				$user_slides.each(function(idx){
+					$slides_nav_links.append('<button class="btn btn-default" data-slide-id="' + idx + '">' + idx + '</button>');
+					$slides.append($user_slides.eq(idx));
+
+					idx == 0 ? $slides_nav_links.find('button').eq(idx).addClass('btn-primary') : $slides.find('li').eq(idx).addClass('hide');
+				});
+                $('#'+this.settings.get('user_slides')).remove();
+                
+
+				$slides_nav_prev.on('click', function(){
+					if(!$(this).hasClass('disabled')){
+						slidesManagr(active_slide_id - 1);
+					}
+				});
+				$slides_nav_next.on('click', function(){
+					if(!$(this).hasClass('disabled')){
+						slidesManagr(active_slide_id + 1);
+					}
+				});
+				$slides_nav_links.on('click', function(e){
+					slidesManagr($(e.target).attr('data-slide-id'));
+				});
+
+
+
+				
+				
+				// 
+				function slidesManagr(slide_id){
+					
+					slide_id = parseInt(slide_id);
+					
+					
+					
+					$slides.find('li').eq(active_slide_id).addClass('hide');
+					$slides_nav_links.find('button').eq(active_slide_id).removeClass('btn-primary')
+					
+					$slides.find('li').eq(slide_id).removeClass('hide');
+					$slides_nav_links.find('button').eq(slide_id).addClass('btn-primary');
+					
+					
+					if(slide_id == 0){
+						if(!$slides_nav_prev.hasClass('disabled')){
+							$slides_nav_prev.addClass('disabled');
+						}
+						
+						if($slides_nav_next.hasClass('disabled')){
+							$slides_nav_next.removeClass('disabled');
+						}
+					}
+					
+					else if(slide_id == max_slide_id){
+						if(!$slides_nav_next.hasClass('disabled')){
+							$slides_nav_next.addClass('disabled');
+						}
+						
+						if($slides_nav_prev.hasClass('disabled')){
+							$slides_nav_prev.removeClass('disabled');
+						}
+					}
+					
+					else{
+						if($slides_nav_prev.hasClass('disabled')){
+							$slides_nav_prev.removeClass('disabled');
+						}
+						if($slides_nav_next.hasClass('disabled')){
+							$slides_nav_next.removeClass('disabled');
+						}
+					}
+
+					active_slide_id = slide_id;					
+				}
+
+				$slides_nav_prev.addClass('disabled');
+				
+				
+				
+				function isScrollNeeded(){
+					var overflow_y = $slides_nav.outerHeight(true) + $slides.outerHeight() > $modal_body.outerHeight() ? 'scroll' : 'none'; 
+					$modal_body.css('overflow-y', overflow_y);
+				};
+				
+				//var ck_name = this.settings.get('no_more_cookie_name');
+				
+				
+				$modal
+				.on('show.bs.modal', function(e){
+					isScrollNeeded();
+				})
+				.on('shown.bs.modal', function(e){
+					// Prevents page body scrolling if modal content is scrollable
+					$('body').css({overflow:"hidden", position:"fixed", width: "100%"});
+				})
+				.on('hide.bs.modal', function(e){
+					//
+					$('body').css({overflow:"inherit", position:"inherit", width: "inherit"});
+					
+					// Never show Tour until
+					if($checkbox.is(':checked')){
+						setCookie(ck_name);
+					}
+				});
+				
+				// 
+				$(window).resize(function(){
+					isScrollNeeded();
+				});
+
+
+				
+				
+				
+				
+				
+				$modal.modal('show');
+				
+			}
+			
+			
+			
+			return this;
+		}
+	});
+	
+	return SccTakeTheTour;
+});
