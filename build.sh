9221c93af72f393ca073c9a5354c0c6275e7a65a
guilhem <guilhem.marchand@gmail.com>
2017-05-20 20:50:27 +0100

    exlude auto backup lookup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PWD=`pwd`
 app="nmon-performance-monitor-for-unix-and-linux-systems"
 version=`grep 'version =' nmon/default/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
-tar -czf ${app}_${version}.tgz nmon --exclude=nmon/local --exclude=nmon/metadata/local.meta
+tar -czf ${app}_${version}.tgz nmon --exclude=nmon/local --exclude=nmon/metadata/local.meta --exclude=nmon/lookups/lookup_file_backups
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
