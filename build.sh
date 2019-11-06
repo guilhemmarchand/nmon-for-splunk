12c7b9057fffc7dec455355a7dbca3cd605d9b64
guilhem <guilhem.marchand@gmail.com>
2019-11-06 19:11:29 +0000

    Version 1.9.20

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PWD=`pwd`
 app="nmon-performance-monitor-for-unix-and-linux-systems"
 version=`grep 'version =' nmon/default/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
-tar -czf ${app}_${version}.tgz nmon --exclude=nmon/local --exclude=nmon/metadata/local.meta --exclude=nmon/lookups/lookup_file_backups
+tar -czf ${app}_${version}.tgz --exclude=nmon/local --exclude=nmon/metadata/local.meta --exclude=nmon/lookups/lookup_file_backups nmon
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
